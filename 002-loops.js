27932cdf809f493ee7f91cdf28cde04e539ff38c
ak-onas <okhalid19@elpuig.xeill.net>
2018-05-08 13:29:28 +0200

    version 5

---
+++ 002-loops.js
@@ -0,0 +1,48 @@
+var elements = [3, 7, 9, 18, 24, 5, 7, 27, 32, 35, 42, 47];
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+function greaterThan18(elements) {
+
+  var total = 0;
+  for (i = 0; i < elements.length; i++) {
+    // Instrucciones
+
+  if (elements[i] > 18 ) {
+  total = total + elements[i];
+  }
+}
+alert(total);
+return total;
+}
+
+document.getElementById("A").innerHTML = greaterThan18(elements);
+
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+// y que además son pares
+function greaterThan18Odd(elements) {
+
+  var total = 0;
+  for (i = 0; i < elements.length; i++) {
+    // Instrucciones
+
+  if (elements[i] > 18 ) {
+  total = total + elements[i];
+  }
+}
+  // Escriviu aquí el vostre codi:
+  return total;
+}
+
+document.getElementById("B").innerHTML = greaterThan18Odd(elements);
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+// y que además son pares y que se encuentran en posiciones impares del vector
+function greaterThan18OddEvenPositions(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+
+document.getElementById("C").innerHTML = greaterThan18OddEvenPositions(elements);
